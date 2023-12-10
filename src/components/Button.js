e250a2f363ede28aee522ae957ee378745ebf2f1
Carmen Mattos <carmencmattos@gmail.com>
2023-12-10 11:57:17 -0300

    feat: add funcionalidades

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ const Button = ({ textValue, compute }) => {
   return (
     <div className="row">
       <div>
-        <a className="default-button" onClick={compute}>
+        <div className="default-button" onClick={compute}>
           {textValue}
-        </a>
+        </div>
       </div>
     </div>
   );
--- src/container/Body.js
+++ src/container/Body.js
@@ -5,8 +5,6 @@ import Items from "../components/Items";
 import { useState } from "react";
 
 const calculationTable = {
-  duracao: 5,
-
   homens: {
     carne: 2,
     paoDeAlho: 1,
@@ -37,10 +35,7 @@ const calculationTable = {
 };
 
 export const Body = () => {
-  const [numHomens, setNumHomens] = useState(0);
-  const [numMulheres, setNumMulheres] = useState(0);
-  const [numCriancas, setNumCriancas] = useState(0);
-  const [consumableItems, setConsumableItems] = useState({
+  const defaultConsumableItems = {
     carne: 0,
     paoDeAlho: 0,
     refrigerante: 0,
@@ -48,20 +43,67 @@ export const Body = () => {
     carvao: 0,
     sal: 0,
     gelo: 0,
-  });
-
+  };
+  const [numHomens, setNumHomens] = useState(0);
+  const [numMulheres, setNumMulheres] = useState(0);
+  const [numCriancas, setNumCriancas] = useState(0);
+  const [consumableItems, setConsumableItems] = useState(
+    defaultConsumableItems
+  );
+  const clear = () => {
+    setNumHomens(0);
+    setNumMulheres(0);
+    setNumCriancas(0);
+    setConsumableItems(defaultConsumableItems);
+  };
   const calculation = () => {
     const numPessoas = numHomens + numMulheres + numCriancas;
     const numCarne =
-      (numHomens * calculationTable.homens.carne +
-        numMulheres * calculationTable.mulheres.carne +
-        numCriancas * calculationTable.criancas.carne) *
-      calculationTable.duracao;
+      numHomens * calculationTable.homens.carne +
+      numMulheres * calculationTable.mulheres.carne +
+      numCriancas * calculationTable.criancas.carne;
+
+    const numPaoDeAlho =
+      numHomens * calculationTable.homens.paoDeAlho +
+      numMulheres * calculationTable.mulheres.paoDeAlho +
+      numCriancas * calculationTable.criancas.paoDeAlho;
+
+    const numRefrigerante =
+      numHomens * calculationTable.homens.refrigerante +
+      numMulheres * calculationTable.mulheres.refrigerante +
+      numCriancas * calculationTable.criancas.refrigerante;
+
+    const numAgua =
+      numHomens * calculationTable.homens.agua +
+      numMulheres * calculationTable.mulheres.agua +
+      numCriancas * calculationTable.criancas.agua;
+
+    const numCarvao =
+      numHomens * calculationTable.homens.carvao +
+      numMulheres * calculationTable.mulheres.carvao +
+      numCriancas * calculationTable.criancas.carvao;
+
+    const numSal =
+      numHomens * calculationTable.homens.sal +
+      numMulheres * calculationTable.mulheres.sal +
+      numCriancas * calculationTable.criancas.sal;
+
+    const numGelo =
+      numHomens * calculationTable.homens.gelo +
+      numMulheres * calculationTable.mulheres.gelo +
+      numCriancas * calculationTable.criancas.gelo;
+
     console.log(numCarne, numPessoas);
     setConsumableItems((prevState) => {
       return {
         ...prevState,
         carne: numCarne,
+        paoDeAlho: numPaoDeAlho,
+        refrigerante: numRefrigerante,
+        agua: numAgua,
+        carvao: numCarvao,
+        sal: numSal,
+        gelo: numGelo,
       };
     });
   };
@@ -90,21 +132,38 @@ export const Body = () => {
             inputValue={numCriancas}
           />
         </div>
+
         <div className="row">
           <Button textValue="Calcular" compute={calculation} />
+          <Button textValue="Limpar" compute={clear} />
         </div>
 
         <div className="input-group-result">
-          <div className="results">
-            <Items
-              item="Carne"
-              amount="0.0kg"
-              consumableItems={consumableItems}
-            />
+          <div id="header-result">
+            <p>ITEM</p>
+            <p>QUANTIDADE</p>
           </div>
-        </div>
-        <div className="row">
-          <Button textValue="Novo cálculo" compute={() => {}} />
+          <div className="results">
+            <Items item="Carne" unit="kg" amount={consumableItems.carne} />
+            <Items
+              item="Pão de alho"
+              unit="unidades"
+              amount={consumableItems.paoDeAlho}
+            />
+            <Items
+              item="Refrigerante"
+              unit="garrafa de 2L"
+              amount={consumableItems.refrigerante}
+            />
+            <Items
+              item="Água"
+              unit="garrafa de 2L"
+              amount={consumableItems.agua}
+            />
+            <Items item="Carvão" unit="kg" amount={consumableItems.carvao} />
+            <Items item="Sal" unit="kg" amount={consumableItems.sal} />
+            <Items item="Gelo" unit="kg" amount={consumableItems.gelo} />
+          </div>
         </div>
       </div>
     </div>
