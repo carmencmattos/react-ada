014fd985a6f314d685a5cdbfe0088bfda9012543
Carmen Mattos <carmencmattos@gmail.com>
2023-12-11 20:50:45 -0300

    refactor: alterando um componente para class component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-export const Header = ({ title, subtitle, info }) => {
-  return (
-    <>
-      <h1 className="style-h3">{title}</h1>
-      <h3>{subtitle}</h3>
-      <h3 className="configSegH3">{info}</h3>
-    </>
-  );
-};
-
+import React, { Component } from "react";
+class Header extends Component {
+  render() {
+    const { title, subtitle, info } = this.props;
+    return (
+      <>
+        <h1 className="style-h3">{title}</h1>
+        <h3>{subtitle}</h3>
+        <h3 className="configSegH3">{info}</h3>
+      </>
+    );
+  }
+}
 export default Header;
